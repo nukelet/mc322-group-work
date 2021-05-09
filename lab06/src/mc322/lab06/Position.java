bb16b5485879795f7da839c5aa013a80bf41a4ad
Gabriel Araujo <glucio314@gmail.com>
2021-05-09 14:53:24 -0300

    [lab06] Add construction methods to Position

--- lab06/src/mc322/lab06/Position.java
+++ lab06/src/mc322/lab06/Position.java
@@ -17,6 +17,22 @@ public class Position {
         return this.column;
     }
 
+    public Position up() {
+        return new Position(row, column - 1);
+    }
+
+    public Position down() {
+        return new Position(row, column + 1);
+    }
+
+    public Position left() {
+        return new Position(row - 1, column);
+    }
+
+    public Position right() {
+        return new Position(row + 1, column);
+    }
+
     public boolean isEqual(Position position) {
         return this.row == position.getRow() &&
             this.column == position.getColumn();

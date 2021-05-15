3dc473b590c02cd9e94a4735a7754206886b5461
Vinícius Peixoto <vinicius241201@gmail.com>
2021-05-15 08:47:03 -0300

    [lab06] Add methods to Room

--- lab06/src/mc322/lab06/Room.java
+++ lab06/src/mc322/lab06/Room.java
@@ -1,6 +1,7 @@
 package mc322.lab06;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import mc322.lab06.components.*;
 
@@ -22,23 +23,27 @@ public class Room {
     }
 
     public ErrorType addComponent(Component component) {
-        ComponentType[] incompatibleTypes = 
-            {
-                    ComponentType.GOLD,
-                    ComponentType.HOLE,
-                    ComponentType.WUMPUS
+        ComponentType[] incompatibleTypes = {
+                ComponentType.GOLD,
+                ComponentType.HOLE,
+                ComponentType.WUMPUS
             };
 
         switch (component.getType()) { 
-        case GOLD:
-        case HOLE:
-        case WUMPUS:
-            if (hasComponentType(incompatibleTypes)) {
-                return ErrorType.INVALID_COMPONENTS;
-            }
-            break;
-        default:
-            components.add(component);
+            case GOLD:
+            case HOLE:
+            case WUMPUS:
+                if (hasComponentType(incompatibleTypes)) {
+                    return ErrorType.INVALID_COMPONENTS;
+                } else {
+                    components.add(component);
+                }
+                break;
+            default:
+                if (component.getType() == ComponentType.HERO) {
+                    setAsVisited();
+                }
+                components.add(component);
         }
 
         return ErrorType.NONE;
@@ -66,17 +71,48 @@ public class Room {
         return this.components;
     }
 
-    public void removeComponent(Component component) {
+    public boolean removeComponent(Component component) {
+        return components.remove(component);
+    }
 
+    public boolean removeComponentType(ComponentType type) {
+        return components.removeIf(c -> c.getType() == type);
     }
 
     public boolean isEmpty() {
         return components.isEmpty();
     }
 
+    public String getComponentString() {
+        // TODO: maybe come up with a better way to do this?
+        HashMap<ComponentType, Integer> priority = new HashMap<ComponentType, Integer>() {{ 
+            put(ComponentType.GOLD, 3);
+            put(ComponentType.WUMPUS, 3);
+            put(ComponentType.HOLE, 3);
+            put(ComponentType.HERO, 2);
+            put(ComponentType.STINK, 1);
+            put(ComponentType.BREEZE, 0);
+        }};
+
+        Component maxPriority = null;
+        for (Component c : components) {
+            if (maxPriority == null) {
+                maxPriority = c;
+            } else if (priority.get(c.getType()) > priority.get(maxPriority.getType())) {
+                maxPriority = c;
+            }
+        }
+
+        String str = maxPriority == null ? "#" : maxPriority.singleLetterCode();
+        return str;
+    }
     @Override
     public String toString() {
         // TODO finish implementing the components part
-        return "Room(components=" + ", visited=" + visited + ")";
+        String str = "";
+        for (Component c : components) {
+            str += c.toString() + "\n";
+        }
+        return "Room(components=" + str + ", visited=" + visited + ")";
     }
 }

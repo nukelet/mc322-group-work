5115fc835a2f7404d89e73403ace4a5f9e080782
Vinícius Peixoto <vinicius241201@gmail.com>
2021-05-15 08:44:42 -0300

    [lab06] Add methods to Hero

--- lab06/src/mc322/lab06/components/Hero.java
+++ lab06/src/mc322/lab06/components/Hero.java
@@ -1,38 +1,87 @@
 package mc322.lab06.components;
 
 import mc322.lab06.Cave;
+import mc322.lab06.Room;
 import mc322.lab06.Position;
 
 public class Hero extends Component {
     private int arrows;
-    private boolean arrowEquiped;
+    private boolean arrowEquipped;
 
     public Hero(Cave cave, Position position) {
         super(cave, position);
         this.arrows = 1;
-        this.arrowEquiped = false;
+        this.arrowEquipped = false;
     }
 
-    public void equipArrow() {
-        if (!arrowEquiped && arrows >= 1) {
+    public boolean isArrowEquipped() {
+        return arrowEquipped;
+    }
+
+    public boolean equipArrow() {
+        if (!arrowEquipped && arrows >= 1) {
             arrows--;
-            arrowEquiped = true;
+            arrowEquipped = true;
+            return true;
+        } else {
+            return false;
         }
     }
 
     public void useArrow() {
-        // TODO finish implementing (if needed)
-        if (arrowEquiped) {
-            arrowEquiped = false;
+        if (arrowEquipped) {
+            arrowEquipped = false;
         }
     }
 
-    public void collectGold() {
-        // TODO implement method
+    /* Moves the Hero component to the specified position
+     * @params: destination
+     * @returns: true if the move was successful and false otherwise
+     */
+    public boolean move(Position destination) {
+        // TODO: consider refactoring this
+        // i think that the fact that we need to do this to move the Component
+        // might mean that we probably don't have the right abstraction
+        Position oldPos = this.position;
+        this.position = destination;
+
+        if (!cave.addComponent(this)) {
+            this.position = oldPos;
+            return false;
+        } else {
+            Room oldPlayerRoom = cave.roomAt(oldPos);
+            oldPlayerRoom.removeComponent(this);
+            return true;
+        }
     }
 
-    @Override
-    protected void spawnSecondaryComponents() {}
+    public boolean moveUp() {
+        Position destination = this.position.up();
+        return move(destination);
+    }
+
+    public boolean moveDown() {
+        Position destination = this.position.down();
+        return move(destination);
+    }
+
+    public boolean moveLeft() {
+        Position destination = this.position.left();
+        return move(destination);
+    }
+
+    public boolean moveRight() {
+        Position destination = this.position.right();
+        return move(destination);
+    }
+
+    /* Collects Gold from the Room the Hero is in
+     * @returns: true if Gold could be collected, false otherwise
+     */
+    public boolean collectGold() {
+        Room currentRoom = cave.roomAt(position);
+        return currentRoom.removeComponentType(ComponentType.GOLD);
+    }
 
     @Override
     public ComponentType getType() {
